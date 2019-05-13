2da48978350871788ac7002b5f3b3f6202340c29
Luquatic <jesseyfransen@live.nl>
2019-05-13 12:22:21 +0200

    updated title:Text

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList'),
+          title: Text('Applaudio!'),
         ),
       ),
     );
