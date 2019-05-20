22543c39726054b7471a9f5e932f26a7c8e1d692
Luquatic <jesseyfransen@live.nl>
2019-05-21 00:40:03 +0200

    added PopupMenuButton

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 
 import '../RecordAudio.dart';
+import '../constants.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageButtonState createState() => _HomePageButtonState();
+}
+
+class _HomePageButtonState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Applaudio!'),
+          actions: <Widget>[
+            PopupMenuButton<String>(itemBuilder: (BuildContext context) {
+              return Constants.choices.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            })
+          ],
         ),
-        body:RecordAudio()
-      );
+        body: RecordAudio());
+  }
+
+  void choiceAction(String choice) {
+    if (choice == Constants.Settings) {
+      print('Settings');
+    }
   }
 }
