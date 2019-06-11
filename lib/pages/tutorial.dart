9821277db7b25ef43faabac4e01e678424bdb85f
Luquatic <jesseyfransen@live.nl>
2019-06-11 22:20:47 +0200

    added dots indicator for pageview

--- lib/pages/tutorial.dart
+++ lib/pages/tutorial.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:dots_indicator/dots_indicator.dart';
+
+import 'package:Applaudio/themes/applaudio.dart' as Theme;
 
 class TutorialPage extends StatefulWidget {
   @override
@@ -8,31 +11,57 @@ class TutorialPage extends StatefulWidget {
 }
 
 class _TutorialPageState extends State<TutorialPage> {
+  int _currentIndexPage;
+  int _pageLength;
   final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    _currentIndexPage = 0;
+    _pageLength = 3;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Applaudio!: Uitleg'),
-      ),
-      body: PageView(
-        controller: _pageController,
-        children: <Widget>[
-          Container(
-            child: Text('Page 1'),
-            color: Colors.red,
+        body: Stack(
+      children: <Widget>[
+        PageView(
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.white,
+            ),
+            Container(
+              color: Colors.blue,
+            ),
+          ],
+          onPageChanged: (value) {
+            setState(() => _currentIndexPage = value);
+          },
+        ),
+        Positioned(
+          top: MediaQuery.of(context).size.height * 0.7,
+          // left: MediaQuery.of(context).size.width * 0.35,
+          child: Padding(
+            padding:
+                EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.35),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: DotsIndicator(
+                dotsCount: _pageLength,
+                position: _currentIndexPage,
+                decorator: DotsDecorator(
+                  activeColor: Theme.ApplaudioColors.lichtGroen[500],
+                ),
+              ),
+            ),
           ),
-          Container(
-            child: Text('Page 2'),
-            color: Colors.white,
-          ),
-          Container(
-            child: Text('Page 3'),
-            color: Colors.blue,
-          ),
-        ],
-      ),
-    );
+        )
+      ],
+    ));
   }
 }
