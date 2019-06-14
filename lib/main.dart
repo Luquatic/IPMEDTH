ff9b29ba8f767c5c895699617326803be37160e9
RamonArents <ramonarents@hotmail.com>
2019-06-14 10:53:32 +0200

    main.dart opgeschoond

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,12 @@ import 'package:Applaudio/themes/applaudio.dart'
     as Theme; //package for the custom styles
 
 import 'package:scoped_model/scoped_model.dart';
-// import 'package:flutter/rendering.dart';
 
 import './pages/intro.dart';
 import './pages/tutorial.dart';
 import './pages/home.dart';
 import './pages/profile.dart';
 import './pages/profile_list.dart';
-import './models/profile.dart';
 import './scoped_models/profiles.dart';
 
 void main() {
